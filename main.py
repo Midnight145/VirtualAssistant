94f98426f5ebd5a0807620a42975f3d4adbc81e5
Midnight <23142346+Midnight145@users.noreply.github.com>
2024-09-25 13:20:36 -0500

    play, pause --> playpause, use .env file instead of finding it automatically

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from dotenv import dotenv_values
+
 from lib import Config, modules, util
 
-config = Config(dotenv_values())
+config = Config(dotenv_values(".env"))
 util.init(config)
 
 user_input = "Lower the volume by 10%"
@@ -9,10 +10,8 @@ prediction_text = util.normalize_string(user_input)
 prediction = util.predict(prediction_text)
 print(prediction)
 if prediction[0][0] == "__label__askweather":
-    modules.weather("What is the weather in Denton, Texas?")
-elif prediction[0][0] == "__label__play":
-    modules.MediaManager.play_pause()
-elif prediction[0][0] == "__label__pause":
+    modules.weather(user_input)
+elif prediction[0][0] == "__label__playpause":
     modules.MediaManager.play_pause()
 elif prediction[0][0] == "__label__raisevolume":
     modules.MediaManager.change_volume(user_input, up=True)
@@ -20,5 +19,3 @@ elif prediction[0][0] == "__label__lowervolume":
     modules.MediaManager.change_volume(user_input, up=False)
 elif prediction[0][0] == "__label__stopplay":
     modules.MediaManager.stopplay()
-
-
